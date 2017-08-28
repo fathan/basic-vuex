754e41d064bfe1878bb2c4e5ea3b874a4101e38c
FathanRohman <doctor.fathan@gmail.com>
2017-08-28 23:54:35 +0700

    refactor module store products

--- src/store/store.js
+++ src/store/store.js
@@ -1,40 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import moduleA from './module/products'
+
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-  state: {
-    products: [
-      {name: 'Toyota Rush Sportivo', price: 2000000},
-      {name: 'Macbook Pro Retina', price: 400000},
-      {name: 'Yamaha V-IXION', price: 600000},
-      {name: 'Helm Racing', price: 800000}
-    ]
-  },
-  getters: {
-    saleProducts: state => {
-      let saleProducts = state.products.map(product => {
-        return {
-          name: '**' + product.name + '**',
-          price: product.price / 2
-        }
-      })
-      return saleProducts
-    }
-  },
-  mutations: {
-    reducePrice: (state, payload) => {
-      state.products.forEach(product => {
-        product.price -= payload
-      })
-    }
-  },
-  actions: {
-    reducePrice: (context, payload) => {
-      setTimeout(function () {
-        context.commit('reducePrice', payload)
-      }, 2000)
-    }
+  modules: {
+    a: moduleA
   }
 })
